11ad104e21007813a4d3521f79749201bc6f6916
Protospace <info@protospace.ca>
2013-12-31 23:50:54 +0000

    Inverted Z-Axis

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -34,31 +34,27 @@
 #define BAUD_RATE 57600
 // #define DEBUG_IGNORE_SENSORS  // set for debugging
 
-
-#ifndef V1401
-  #define CONFIG_X_STEPS_PER_MM 32.80839895 //microsteps/mm
-  #define CONFIG_Y_STEPS_PER_MM 32.80839895 //microsteps/mm
-  #define CONFIG_Z_STEPS_PER_MM 32.80839895 //microsteps/mm
+#define CONFIG_X_STEPS_PER_MM 39.37007874 //microsteps/mm
+#ifndef NANOTEC_STEPPER_09
+  #define CONFIG_Y_STEPS_PER_MM 39.37007874 //microsteps/mm
 #else
-  #define CONFIG_X_STEPS_PER_MM 88.88888888 //microsteps/mm
-  #define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm
-  #define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm
+  #define CONFIG_Y_STEPS_PER_MM 39.37007874 //microsteps/mm
 #endif
+#define CONFIG_Z_STEPS_PER_MM 1000 //microsteps/mm
+
 #define CONFIG_PULSE_MICROSECONDS 5
-#define CONFIG_FEEDRATE 8000.0 // in millimeters per minute
-#define CONFIG_SEEKRATE 8000.0
-#define CONFIG_ACCELERATION 1800000.0 // mm/min^2, typically 1000000-8000000, divide by (60*60) to get mm/sec^2
+#define CONFIG_FEEDRATE 2000.0 // in millimeters per minute
+#define CONFIG_SEEKRATE 2000.0
+#define CONFIG_ACCELERATION 4000000 // mm/min^2, typically 1000000-8000000, divide by (60*60) to get mm/sec^2
+									// Appears to have minimal effect on cut quality. set to middle value of 4e6
 #define CONFIG_JUNCTION_DEVIATION 0.006 // mm
 #define CONFIG_X_ORIGIN_OFFSET 5.0  // mm, x-offset of table origin from physical home
 #define CONFIG_Y_ORIGIN_OFFSET 5.0  // mm, y-offset of table origin from physical home
 #define CONFIG_Z_ORIGIN_OFFSET 0.0   // mm, z-offset of table origin from physical home
-#ifndef V1401
-  #define CONFIG_INVERT_X_AXIS 1  // 0 is regular, 1 inverts the y direction
-#else
-  #define CONFIG_INVERT_X_AXIS 0
-#endif
+
+#define CONFIG_INVERT_X_AXIS 1  // 0 is regular, 1 inverts the x direction
 #define CONFIG_INVERT_Y_AXIS 1  // 0 is regular, 1 inverts the y direction
-#define CONFIG_INVERT_Z_AXIS 1  // 0 is regular, 1 inverts the y direction
+#define CONFIG_INVERT_Z_AXIS 0  // 0 is regular, 1 inverts the z direction
 
 
 #define SENSE_DDR               DDRD
@@ -152,12 +148,12 @@
 // round-off can be great enough to cause problems and/or it's too fast for the Arduino. The correct
 // value for this parameter is machine dependent, so it's advised to set this only as high as needed.
 // Approximate successful values can range from 30L to 100L or more.
-#define ACCELERATION_TICKS_PER_SECOND 200L
+#define ACCELERATION_TICKS_PER_SECOND 120L
 
 // Minimum planner junction speed. Sets the default minimum speed the planner plans for at the end
 // of the buffer and all stops. This should not be much greater than zero and should only be changed
 // if unwanted behavior is observed on a user's machine when running at very slow speeds.
-#define ZERO_SPEED 0.0 // (mm/min)
+#define ZERO_SPEED 10.0 // (mm/min)
 
 // Minimum stepper rate. Sets the absolute minimum stepper rate in the stepper program and never runs
 // slower than this value, except when sleeping. This parameter overrides the minimum planner speed.
